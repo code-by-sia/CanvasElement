7aa48a1c31da80a0d8fcbf8523b45f866fd90dc9
syamandmaroufi <siamandmaroufi@gmail.com>
2015-02-12 01:39:34 +0330

    add Rectangle Element

--- js/sample.js
+++ js/sample.js
@@ -1,9 +1,18 @@
+function random(start,end){
+	return Math.floor(Math.random() * (end - start)) + start;
+}
+
+function randomColor () {
+	return "#"+((1<<24)*Math.random()|0).toString(16);
+}
+
 $(document).ready(function(){
 	var el = document.getElementById('canvas1');
-	var canvasEl= new CanvasElement(el);
+	window.canvasEl= new CanvasElement(el);
 
 	$('#btnTest').click(function(){
-		
+		var rect = new Rectangle(random(0,400),random(0,500),random(100,300),random(100,200),randomColor(),randomColor());
+		canvasEl.insertElement(rect);
 	});
 
 	$('#btnRepaint').click(function () {
--- lib/canvasElement.jsx
+++ lib/canvasElement.jsx
@@ -6,6 +6,13 @@ class CanvasElement{
 		this.height= el.height;
 		this.backgroundColor = 'white';
 		this.backgroundImage = null;
+		this.elements = [];
+	}
+
+	insertElement(elemet){
+		elemet.setPainter(this);
+		this.elements.push(elemet);
+		this.repaint();
 	}
 
 	repaint(){
@@ -18,6 +25,49 @@ class CanvasElement{
 		if (this.backgroundImage) {
 			context.drawImage(this.backgroundImage,0,0,this.width,this.height);
 		}
+		for (let i = 0; i < this.elements.length; i++) {
+			let element = this.elements[i];
+			element.paint();
+		};
+	}
 
+
+}
+
+class DrawableElement {
+	constructor(x,y,w,h,fill='',stroke=''){
+		this.x=x;
+		this.y=y;
+		this.width=w;
+		this.height=h;
+		this.fill=fill;
+		this.stroke=stroke;
+	}
+
+	setPainter(painter){
+		this.painter = painter;
+	}
+
+	paint(){
+		let context = this.painter.context;
+		context.setTransform(1, 0, 0, 1, 0, 0);
+		context.translate(this.x, this.y);
+		context.fillStyle= this.fill;
+		context.strokeStyle= this.stroke;
+	}
+}
+
+class Rectangle extends DrawableElement {
+	constructor(x,y,w,h,fill,stroke){
+		super(x,y,w,h,fill,stroke);
+	}
+
+	paint(){
+		super.paint();
+		let context = this.painter.context;
+		if(this.fill)
+			context.fillRect(0,0,this.width,this.height);
+		if(this.stroke)
+			context.strokeRect(0,0,this.width,this.height);
 	}
 }
